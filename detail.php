611de8190f99b51cecb5db83c26baab1e0895862
Pablo Suchmans <suchmanspablo@gmail.com>
2020-08-24 15:34:41 -0300

    first2

--- detail.php
+++ detail.php
@@ -207,7 +207,7 @@
                                               "pending" => "https://mercadopago-certificado.herokuapp.com/pendiente.php"
                                           );
                                           $preference->auto_return = "approved";
-                                          $preference->notification_url = "https://www.creadoresdesoft.com.ar/mercadopagopablo/mp.php";
+                                          $preference->notification_url = "https://www.cooperativaaguapb.com.ar/mercadopago/mp.php";
                                           //$preference->notification_url = "https://certificado-mercadopago.herokuapp.com/notifications.php";
                                         
 
